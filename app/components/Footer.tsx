79de293381afee32cc914f5a55e66545f4642ec6
ranketgit <jalal@ranket.co>
2026-03-31 19:52:21 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span>Résidence AL ANDALOUS, Lotissement Ouled Taleb (Parallèle Californie, Bd. Al Qods), Casablanca.</span>
+                <span>Résidence AL ANDALOUS, Taddart, Californie, Casablanca.</span>
               </li>
               <li className="flex items-center gap-3 text-[15px] text-gray-500">
                 <svg className="w-5 h-5 text-[#E38F75] flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
