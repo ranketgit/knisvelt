dd2d59061e82bf87cae9a8ba0e09e59a872efd10
ranketgit <jalal@ranket.co>
2026-05-11 23:03:27 +0100

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,6 +19,25 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 transition-all duration-300">
+      <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '1688202398979613'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=1688202398979613&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Meta Pixel Code -->
       <div className="bg-[#694e6e] text-white/90 py-2 border-b border-white/5">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center text-[13px] font-medium">
           <div className="hidden sm:flex items-center gap-6">
